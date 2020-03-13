f4fb1905a106dfbc3e9e544eb73f575846a0517e
carloskita <carloskitazawa@hotmail.com>
2020-03-13 17:48:51 -0300

    agr vai lesk

---
+++ Assets/Scripts/store.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class store : MonoBehaviour
+{
+
+    int StoreCount;
+    public Text StoreCountText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StoreCount = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BuyStoreOnClick()
+    {
+
+        StoreCount += 1;
+        Debug.Log(StoreCount);
+        StoreCountText.text = StoreCount.ToString();
+
+    }
+}
