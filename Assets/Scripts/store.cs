b2ee9065dae3c4d4b7e10924e44d194de41257a0
carloskita <carloskitazawa@hotmail.com>
2020-03-19 17:18:01 -0300

    haha

--- Assets/Scripts/store.cs
+++ Assets/Scripts/store.cs
@@ -8,10 +8,16 @@ public class store : MonoBehaviour
 
     float CurrentBalance;
     float BaseStoreCost;
+    float BaseStoreProfit;
 
     int StoreCount;
     public Text StoreCountText;
     public Text CurrentBalanceText;
+    public Slider ProgressSlider;
+
+    float StoreTimer = 4f;
+    float CurrentTimer = 0;
+    bool StartTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +26,30 @@ public class store : MonoBehaviour
         StoreCount = 1;
         CurrentBalance = 2.0f;
         BaseStoreCost = 1.50f;
+        BaseStoreProfit = .5f;
         CurrentBalanceText.text = CurrentBalance.ToString("C2");
+        StartTimer = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (StartTimer)
+        {
+            CurrentTimer += Time.deltaTime;
+            if (CurrentTimer > StoreTimer)
+            {
+                Debug.Log("Time has ended. Reset.");
+                StartTimer = false;
+                CurrentTimer = 0f;
+                CurrentBalance += BaseStoreProfit * StoreCount;
+                CurrentBalanceText.text = CurrentBalance.ToString("C2");
+            }
+            ProgressSlider.value = CurrentTimer / StoreTimer;
+        }
+        ProgressSlider.value = CurrentTimer / StoreTimer;
     }
 
     public void BuyStoreOnClick()
@@ -43,6 +65,16 @@ public class store : MonoBehaviour
         CurrentBalance -= BaseStoreCost;
         CurrentBalanceText.text = CurrentBalance.ToString("C2");
         Debug.Log(CurrentBalance);
+
+    }
+
+    public void StoreOnClick()
+    {
+        Debug.Log("Clicked the store");
+        if (!StartTimer)
+        {
+            StartTimer = true;
+        }
 
     }
 }
